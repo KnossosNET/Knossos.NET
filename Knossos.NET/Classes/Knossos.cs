6c38d565dfc5b971eac6b0a1cb8634b5483ac150
Salvador Cipolla <shivanuo@hotmail.com>
2023-10-18 19:14:17 -0300

    Add height limit to mod cleanup window, bump to 0.1.4-beta3

--- Knossos.NET/Classes/Knossos.cs
+++ Knossos.NET/Classes/Knossos.cs
@@ -21,7 +21,7 @@ namespace Knossos.NET
 {
     public static class Knossos
     {
-        public static readonly string AppVersion = "0.1.4-Beta2";
+        public static readonly string AppVersion = "0.1.4-Beta3";
         private static List<Mod> installedMods = new List<Mod>();
         private static List<FsoBuild> engineBuilds = new List<FsoBuild>();
         public static GlobalSettings globalSettings = new GlobalSettings();
