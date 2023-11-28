ec1e331d1b795bf51a038e800b67fdf88940febe
Salvador Cipolla <shivanuo@hotmail.com>
2023-11-28 18:56:44 -0300

    Remove span html, remove list hack
    It looks like it was the span element what was breaking lists

--- Knossos.NET/Classes/BBCode.cs
+++ Knossos.NET/Classes/BBCode.cs
@@ -101,11 +101,11 @@ namespace BBcodes
                     _rules.Add(new BBCodeRules("[br]", "<br>", ""));
                     _rules.Add(new BBCodeRules("[hr]", "<hr><br>", ""));
 
-                    _rules.Add(new BBCodeRules("[list]", "<ul>", ""));
+                    _rules.Add(new BBCodeRules("[list]", "<ul style='padding-top:-30px;'>", ""));
                     _rules.Add(new BBCodeRules("[/list]", "</ul>", ""));
-                    _rules.Add(new BBCodeRules("[ul]", "<ul>", ""));
+                    _rules.Add(new BBCodeRules("[ul]", "<ul style='padding-top:-30px;'>", ""));
                     _rules.Add(new BBCodeRules("[/ul]", "</ul>", ""));
-                    _rules.Add(new BBCodeRules("[ol]", "<ol>", ""));
+                    _rules.Add(new BBCodeRules("[ol]", "<ol style='padding-top:-30px;'>", ""));
                     _rules.Add(new BBCodeRules("[/ol]", "</ol>", ""));
                     _rules.Add(new BBCodeRules("[li]", "<li>", ""));
                     _rules.Add(new BBCodeRules("[/li]", "</li>", ""));
@@ -212,7 +212,6 @@ namespace BBcodes
             //input = input.Replace("\r\n", "<br />");
             //input = input.Replace("\r", "<br />");
             //input = input.Replace("\n", "<br />");
-            input = input.Replace("[*]", "<br />");
             return input;
         }
 
