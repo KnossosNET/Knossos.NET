35a70d53fa74a2b30e0811587e5c41ac7edaf756
John Fernandez <jafernandez.tampabay@gmail.com>
2023-12-04 11:38:50 -0500

    Fix FaqQuestions class

--- Knossos.NET/Classes/FaqQuestion.cs
+++ Knossos.NET/Classes/FaqQuestion.cs
@@ -1,20 +1,26 @@
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Knossos.NET.Classes{
 
 
-    public class QuestionCategory
+    public partial class QuestionCategory  : ObservableObject
     {
         public string Name { get; set; } = string.Empty;
-        public Question[]? Questions { get; set; }
+
+        public partial class FAQQuestion
+        {        
+            [JsonPropertyName("Question")]
+            public string question { get; set; } = string.Empty;
+            public string Answer { get; set; } = string.Empty;
+        }
+
+        [ObservableProperty]
+        public ObservableCollection<FAQQuestion> questions = new ObservableCollection<FAQQuestion>();
     }
 
 
-    public class Question
-    {        
-        [JsonPropertyName("Question")]
-        public string question { get; set; } = string.Empty;
-        public string Answer { get; set; } = string.Empty;
-    }
 }
