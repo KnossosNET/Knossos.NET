f233381f9752283cad79123d0ebe0dd0ba904c01
Salvador Cipolla <shivanuo@hotmail.com>
2023-12-23 19:33:48 -0300

    Bump to 0.2.0-RC8

--- Knossos.NET/Classes/Knossos.cs
+++ Knossos.NET/Classes/Knossos.cs
@@ -16,7 +16,7 @@ namespace Knossos.NET
 {
     public static class Knossos
     {
-        public static readonly string AppVersion = "0.2.0-RC7";
+        public static readonly string AppVersion = "0.2.0-RC8";
         public readonly static string ToolRepoURL = "https://raw.githubusercontent.com/KnossosNET/Knet-Tool-Repo/main/knet_tools.json";
         public readonly static string GitHubUpdateRepoURL = "https://api.github.com/repos/KnossosNET/Knossos.NET";
         public readonly static string FAQURL = "https://raw.githubusercontent.com/KnossosNET/KNet-General-Resources-Repo/main/communityfaq.json";
