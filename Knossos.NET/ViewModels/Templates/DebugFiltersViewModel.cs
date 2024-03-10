1a7cf2920c5fb12cb40000e0841f3145093dadb0
JohnAFernandez <jafernandez.tampabay@gmail.com>
2024-03-10 11:37:35 -0400

    Use the correct Folder For the filter

--- Knossos.NET/ViewModels/Templates/DebugFiltersViewModel.cs
+++ Knossos.NET/ViewModels/Templates/DebugFiltersViewModel.cs
@@ -64,95 +64,81 @@ namespace Knossos.NET.ViewModels
                 }
             }
 
-            // Load info from any valid config
-            bool configFound = false;
-
-            foreach(var mod in Knossos.GetInstalledModList(null))
-            {
-                try {
-                    // Debug filter files are always in the data folder.
-                    var path = Path.Combine(mod.fullPath, "data", "debug_filter.cfg");
-                    if (File.Exists(path)) 
+            try {
+                // Debug filter files are always in the data folder.
+                var path = Path.Combine(KnUtils.GetFSODataFolderPath(), "data", "debug_filter.cfg");
+                if (File.Exists(path)) 
+                {
+                    using (StreamReader reader = new StreamReader(Path.Combine(KnUtils.GetFSODataFolderPath(), "data", "debug_filter.cfg"))) 
                     {
-                        configFound = true;
-                        using (StreamReader reader = new StreamReader(Path.Combine(mod.fullPath, "data", "debug_filter.cfg"))) 
+                        string? line;
+                        while ((line = reader.ReadLine()) != null)
                         {
-                            string? line;
-                            while ((line = reader.ReadLine()) != null)
-                            {
-                                bool enable = true;
-                                string option = string.Empty;
+                            bool enable = true;
+                            string option = string.Empty;
 
-                                // Treat it just like FSO does.  Use the first character to tell if it's enabled or disabled.
-                                if (line.StartsWith("+"))
+                            // Treat it just like FSO does.  Use the first character to tell if it's enabled or disabled.
+                            if (line.StartsWith("+"))
+                            {
+                                enable = true;
+                                option = line.Substring(1).Trim();
+                            } else if (line.StartsWith("-"))
+                            {
+                                enable = false;
+                                option = line.Substring(1).Trim();
+                            }
+
+                            if (option != string.Empty)
+                            {
+                                bool found = false;
+                                foreach (var category in Categories)
                                 {
-                                    enable = true;
-                                    option = line.Substring(1).Trim();
-                                } else if (line.StartsWith("-"))
-                                {
-                                    enable = false;
-                                    option = line.Substring(1).Trim();
+                                    foreach (var filter in category.Filters)
+                                    {
+                                        if (filter.Filter.Trim().ToLower() == option.ToLower())
+                                        {
+                                            filter.Enabled = enable;
+                                            found = true;
+                                            break;
+                                        }
+                                        
+                                    }
+                                    if (found) 
+                                    {
+                                        break;
+                                    }
                                 }
 
-                                if (option != string.Empty)
+                                // if we didn't have a match in the standard categories
+                                if (!found)
                                 {
-                                    bool found = false;
                                     foreach (var category in Categories)
                                     {
-                                        foreach (var filter in category.Filters)
+                                        if (category.Typename == "Custom")
                                         {
-                                            if (filter.Filter.Trim().ToLower() == option.ToLower())
-                                            {
-                                                filter.Enabled = enable;
-                                                found = true;
-                                                break;
-                                            }
-                                            
-                                        }
-                                        if (found) 
-                                        {
+                                            found = true;
+                                            category.AddCustom(option, enable);
                                             break;
                                         }
                                     }
+                                }
 
-                                    // if we didn't have a match in the standard categories
-                                    if (!found)
-                                    {
-                                        foreach (var category in Categories)
-                                        {
-                                            if (category.Typename == "Custom")
-                                            {
-                                                found = true;
-                                                category.AddCustom(option, enable);
-                                                break;
-                                            }
-                                        }
-                                    }
-
-                                    // if we also didn't have a custom category setup.
-                                    if (!found)
-                                    {
-                                        var customCategory = new DebugFilterCategory();
-                                        customCategory.Typename = "Custom";
-                                        customCategory.AddCustom(option, enable); 
-                                        Categories.Insert(0, customCategory);
-                                    }
+                                // if we also didn't have a custom category setup.
+                                if (!found)
+                                {
+                                    var customCategory = new DebugFilterCategory();
+                                    customCategory.Typename = "Custom";
+                                    customCategory.AddCustom(option, enable); 
+                                    Categories.Insert(0, customCategory);
                                 }
                             }
                         }
-                    }                
-                }
-                catch(Exception ex)
-                {
-                    // retry
-                    configFound = false;
-                    Log.Add(Log.LogSeverity.Warning, "DebugFiltersViewModel.LoadDebugRepo()", ex);
-                }
-                // Only process the first valid config
-                if (configFound)
-                {
-                    break;
-                }
+                    }
+                }                
+            }
+            catch(Exception ex)
+            {
+                Log.Add(Log.LogSeverity.Warning, "DebugFiltersViewModel.LoadDebugRepo()", ex);
             }
 
             Instance = this;
@@ -198,69 +184,65 @@ namespace Knossos.NET.ViewModels
         }
 
         public void SaveConfig(){
-            foreach(var mod in Knossos.GetInstalledModList(null))
+            try 
             {
-                if (mod.type == ModType.tc) {
-                    try 
+                if(!Directory.Exists(Path.Combine(KnUtils.GetFSODataFolderPath(), "data")))
+                {
+                    Directory.CreateDirectory(Path.Combine(KnUtils.GetFSODataFolderPath(), "data"));
+                }
+
+                // much easier to start over with settings the user has chosen
+                var path = Path.Combine(KnUtils.GetFSODataFolderPath(), "data", "debug_filter.cfg");
+                if (File.Exists(path)) {
+                    File.Delete(path);
+                }
+
+                using (StreamWriter sw = new StreamWriter(path, false, new UTF8Encoding(false)))
+                {
+                    foreach(var category in Categories)
                     {
-                        // Debug filter files are always in the data folder.
-                        if(!Directory.Exists(Path.Combine(mod.fullPath, "data")))
+                        // Every custom filter must be added or it will be lost.
+                        if (category.Typename == "Custom") 
                         {
-                            Directory.CreateDirectory(Path.Combine(mod.fullPath, "data"));
-                        }
-
-                        // much easier to start over with settings the user has chosen
-                        var path = Path.Combine(mod.fullPath, "data", "debug_filter.cfg");
-                        if (File.Exists(path)) {
-                            File.Delete(path);
-                        }
-
-                        using (StreamWriter sw = new StreamWriter(path, false, new UTF8Encoding(false)))
-                        {
-                            foreach(var category in Categories)
+                            foreach(var filter in category.Filters)
                             {
-                                // Every custom filter must be added or it will be lost.
-                                if (category.Typename == "Custom") 
-                                {
-                                    foreach(var filter in category.Filters)
-                                    {
-                                        if (filter.Enabled) {
-                                            sw.WriteLine("+" + filter.Filter);
-                                        } else {
-                                            sw.WriteLine("-" + filter.Filter);
-                                        }
-                                    }
-                                } 
-                                // General are on by default, so only list if they are disabled
-                                else if (category.Typename == "General")
-                                {
-                                    foreach(var filter in category.Filters)
-                                    {
-                                        if (!filter.Enabled) 
-                                        {
-                                            sw.WriteLine("-" + filter.Filter);
-                                        }
-                                    }
+                                if (filter.Enabled) {
+                                    sw.WriteLine("+" + filter.Filter);
+                                } else {
+                                    sw.WriteLine("-" + filter.Filter);
                                 }
-                                else
+                            }
+                        } 
+                        // General are on by default, so only list if they are disabled
+                        else if (category.Typename == "General")
+                        {
+                            foreach(var filter in category.Filters)
+                            {
+                                if (!filter.Enabled) 
                                 {
-                                    foreach(var filter in category.Filters)
-                                    {
-                                        if (filter.Enabled)
-                                        {
-                                            sw.WriteLine("+" + filter.Filter);
-                                        }
-                                    }
+                                    sw.WriteLine("-" + filter.Filter);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            foreach(var filter in category.Filters)
+                            {
+                                if (filter.Enabled)
+                                {
+                                    sw.WriteLine("+" + filter.Filter);
                                 }
                             }
                         }
                     }
-                    catch(Exception ex)
-                    {
-                        Log.Add(Log.LogSeverity.Error, "DebugFiltersViewModel.SaveConfig() is unable to save: ", ex);
-                    }
                 }
             }
+            catch(Exception ex)
+            {
+                Log.Add(Log.LogSeverity.Error, "DebugFiltersViewModel.SaveConfig() is unable to save: ", ex);
+            }
+        
+            
         }
     }
 }
