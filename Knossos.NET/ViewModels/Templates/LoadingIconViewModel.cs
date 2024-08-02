1d52406df317517c10ee579a650e4be5b8606bf5
JohnAFernandez <jafernandez.tampabay@gmail.com>
2024-08-02 13:12:29 -0400

    Adapt to new Animation LIbrary

--- Knossos.NET/ViewModels/Templates/LoadingIconViewModel.cs
+++ Knossos.NET/ViewModels/Templates/LoadingIconViewModel.cs
@@ -7,31 +7,5 @@ namespace Knossos.NET.ViewModels
 {
     public partial class LoadingIconViewModel : ViewModelBase
     {
-        [ObservableProperty]
-        internal bool frame0 = true;
-        [ObservableProperty]
-        internal bool frame1 = false;
-
-        public LoadingIconViewModel() 
-        {
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = 500;
-            timer.Elapsed += Animate;
-            timer.Start();
-        }
-
-        private void Animate(object? _, System.Timers.ElapsedEventArgs __)
-        {
-            if(Frame0)
-            {
-                Frame0 = false;
-                Frame1 = true;
-            }
-            else
-            {
-                Frame1 = false;
-                Frame0 = true;
-            }
-        }
     }
 }
