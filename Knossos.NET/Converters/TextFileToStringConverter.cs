2723ea0523a541778e6a6b89b9fe0ea3abe81184
Salvador Cipolla <shivanuo@hotmail.com>
2025-01-17 22:22:11 -0300

    Custom home screen part 5

---
+++ Knossos.NET/Converters/TextFileToStringConverter.cs
@@ -0,0 +1,64 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+using Avalonia.Platform;
+using System.IO;
+using System.Text;
+
+namespace Knossos.NET.Converters
+{
+    public class TextFileToStringConverter : IValueConverter
+    {
+        public static TextFileToStringConverter Instance { get; } = new();
+
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value == null) return null;
+
+                if (value is not string rawUri || !targetType.IsAssignableFrom(typeof(String)))
+                {
+                    throw new NotSupportedException();
+                }
+
+                Uri uri;
+
+                if (rawUri.StartsWith("avares://"))
+                {
+                    uri = new Uri(rawUri);
+                    var asset = AssetLoader.Open(uri);
+                    if (asset != null)
+                    {
+                        using (var reader = new StreamReader(asset, Encoding.UTF8))
+                        {
+                            return reader.ReadToEnd();
+                        }
+                    }
+                }
+                else if(rawUri.ToLower().StartsWith("http"))
+                {
+                    return null;
+                }
+                else if (File.Exists(Path.Combine(KnUtils.GetKnossosDataFolderPath(), rawUri)))
+                {
+                    return File.ReadAllText(Path.Combine(KnUtils.GetKnossosDataFolderPath(), rawUri));
+                }
+                else if (File.Exists(rawUri))
+                {
+                    return File.ReadAllText(rawUri);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Add(Log.LogSeverity.Error, "BitmapAssetValueConverter.Convert()", ex);
+            }
+            return null;
+        }
+
+        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
