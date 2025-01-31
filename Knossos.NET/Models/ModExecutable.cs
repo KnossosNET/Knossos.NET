e2ba6ff8d35f6b14184c45df4b2ba6a96460f848
Salvador Cipolla <shivanuo@hotmail.com>
2025-01-31 00:56:37 -0300

    Move additional Mod classes outside mod.cs

---
+++ Knossos.NET/Models/ModExecutable.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Knossos.NET.Models
+{
+    public class ModExecutable
+    {
+        public string? file { get; set; } // required, path to the executable (*.exe file on Windows), relative to the mod folder
+        public string? label { get; set; } // <Fred FastDebug|FRED2|QTFred|QTFred FastDebug|FastDebug|null> optional, should be null for release builds and contain the name for others
+        public ModProperties? properties { get; set; }
+
+        [JsonIgnore]
+        public int score { get; set; } = 0;
+    }
+}
---
+++ Knossos.NET/Models/ModMember.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knossos.NET.Models
+{
+    /// <summary>
+    /// Nebula int values, do not change
+    /// It is not clear what can each one of the roles do and what not
+    /// </summary>
+    public enum ModMemberRole
+    {
+        Owner = 0,
+        Manager = 10,
+        Uploader = 20,
+        Tester = 30
+    }
+
+    public class ModMember
+    {
+        public ModMemberRole role { get; set; }
+        public string user { get; set; } = string.Empty;
+    }
+}
