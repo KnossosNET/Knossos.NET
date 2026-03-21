092399afa35136ae788f225b3dea2a41f78b9e9e
Salvador Cipolla <shivanuo@hotmail.com>
2026-03-21 18:51:48 -0300

    hide system bars

--- Knossos.NET.Android/MainActivity.cs
+++ Knossos.NET.Android/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.Content.PM;
 using Android.Views;
+using AndroidX.Core.View;
 using Avalonia;
 using Avalonia.Android;
 
@@ -22,10 +23,44 @@ public class MainActivity : AvaloniaMainActivity<App>
             .WithInterFont();
     }
 
+    private void hideSystemUI()
+    {
+        WindowCompat.SetDecorFitsSystemWindows(Window, false);
+
+        var windowInsetsController = WindowCompat.GetInsetsController(Window, Window?.DecorView);
+
+        if (windowInsetsController != null)
+        {
+            // Hide both navigation and status bars
+            windowInsetsController.Hide(WindowInsetsCompat.Type.SystemBars());
+
+            // Or only navigation bars:
+            //windowInsetsController.Hide(WindowInsetsCompat.Type.NavigationBars());
+
+            // Set behavior to show bars temporarily on swipe
+            windowInsetsController.SystemBarsBehavior = WindowInsetsControllerCompat.BehaviorShowTransientBarsBySwipe;
+        }
+    }
+
+    public override void OnWindowFocusChanged(bool hasFocus)
+    {
+        base.OnWindowFocusChanged(hasFocus);
+        if(hasFocus)
+            hideSystemUI();
+    }
+
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         Window?.AddFlags(WindowManagerFlags.KeepScreenOn);
         //Window?.AddFlags(WindowManagerFlags.Fullscreen);
+
         base.OnCreate(savedInstanceState);
+        hideSystemUI();
+    }
+
+    protected override void OnResume()
+    {
+        base.OnResume();
+        hideSystemUI();
     }
 }
