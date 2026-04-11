3b0b189e4768adf4e018d3a8554943c10f92a8df
Salvador Cipolla <shivanuo@hotmail.com>
2026-04-11 17:54:28 -0300

    FileStream Leak on Zero-Length File (M8 fix)

--- Knossos.NET/Classes/KnUtils.cs
+++ Knossos.NET/Classes/KnUtils.cs
@@ -676,8 +676,11 @@ namespace Knossos.NET
                 if (localFile != null)
                 {
                     var fileStream = new FileStream(localFile, FileMode.Open, FileAccess.Read, FileShare.Read);
-                    if(fileStream.Length == 0)
+                    if (fileStream.Length == 0)
+                    { 
+                        fileStream.Dispose();
                         return null;
+                    }
                     return fileStream;
                 }
             }
